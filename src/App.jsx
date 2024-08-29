4f3089c8d86b02ae0ab466289fc0474e6bda0433
Matias Salazar <matias39974593@gmail.com>
2024-08-29 15:31:40 -0300

    se borraron las carpetas que no ibamos a utilizar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+
+  return <section className='container my-5'>
+    <h1 className='text-center'>Hello World!</h1>
+  </section>
+}
+
+export default App
