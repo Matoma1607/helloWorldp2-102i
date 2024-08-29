6eb37ac38d5b3609bda9e90e3b9ce5680582298e
Matias Salazar <matias39974593@gmail.com>
2024-08-29 20:28:16 -0300

    se añadio props para que aparezca un nuevo mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/informacion";
 
-  return <section className='container my-5'>
-    <h1 className='text-center'>Hello World!</h1>
-  </section>
+function App() {
+  return (
+    <section className="container my-5">
+      <h1 className="text-center">Hello World!</h1> <hr />
+      <Informacion Hello="my friend!" />
+    </section>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Informacion = (props) => {
+    return (
+        <div>
+            <h1>Hello {props.Hello}</h1>
+        </div>
+    );
+};
+
+export default Informacion;
